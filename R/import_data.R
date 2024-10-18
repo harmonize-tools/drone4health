d2b271c5dd525d618c3c4e11e7e305c7ee86004d
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-10-18 14:10:53 -0500

    project initiated 🚀

---
+++ R/import_data.R
@@ -0,0 +1,12 @@
+#' Import data
+#'
+#' This functions import database from duckdb
+#'
+#' @param type default is null.
+#'
+#' @return A string.
+
+import_data <- function(type = NULL) {
+  print("Hello, world!")
+}
+
